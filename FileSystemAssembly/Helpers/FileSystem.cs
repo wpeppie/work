117f4415cffbb9f557504947d62a51232d0edbb0
Adrie Jongeling <w.peppie.1972@hotmail.com>
2015-12-09 00:29:51 +0100

    Added Assembly

---
+++ FileSystemAssembly/Helpers/FileSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileSystemAssembly.Helpers
+{
+    public class FileSystem: Interface.IFileSystem
+    {
+        bool Interface.IFileSystem.FileExists(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool Interface.IFileSystem.FolderExists(string folderName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> ReadFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public object ReadXml(string configFileName, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+        public void WriteFile(string fileName, string[] outputLines)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
